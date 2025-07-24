aced832efbf33e7ea2a80edc1d05ed293cb4224e
Fabian Schiebel <fabian.schiebel@iem.fraunhofer.de>
2025-07-24 17:55:53 +0200

    Fix ITST_ASSERT_FAIL_MSGF

--- include/itst/Macros.h
+++ include/itst/Macros.h
@@ -107,8 +107,8 @@ static constexpr bool assertOpImpl(Op op, bool first, bool second) noexcept {
   } while (false)
 #define ITST_ASSERT_FAIL_MSGF(FMT, ...)                                        \
   do {                                                                         \
-    ITST_LOG(Fatal, "At {}:{}: Assertion failed" FMT, __FILE__, __LINE__,      \
-             ##__VA_ARGS__);                                                   \
+    ITST_LOGF(Fatal, "At {}:{}: Assertion failed" FMT, __FILE__, __LINE__,     \
+              ##__VA_ARGS__);                                                  \
     ITST_ASSERT_FAIL();                                                        \
   } while (0)
 
